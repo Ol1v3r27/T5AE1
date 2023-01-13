2ef6ebf1e01f5d2ad920a50e8a8c8b060cd3ecff
Óliver Puchades Pinel <114495024+Ol1v3r27@users.noreply.github.com>
2023-01-13 17:26:10 +0100

    Operació 2.sh

---
+++ Operació 2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Introduce un mes del año: " mes
+diasmes=`cat usuarios.txt | grep $mes | awk '{ print $2 }'`
+
+
+if [ $diasmes = `` ]
+then
+echo "En $mes no han habido conexiones"
+else
+echo "========================="
+echo "En $mes los días son: "
+echo $diasmes
+echo "========================="
+fi
