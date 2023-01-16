b4263362d29595d4fee6860721d2f5bf5be1e882
Óliver Puchades Pinel <114495024+Ol1v3r27@users.noreply.github.com>
2023-01-16 01:34:56 +0100

    Operació 3.sh

---
+++ Operació 3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read -p "Introduce un dia: " dia
+read -p "Introduce un mes: " mes
+usuario=`cat usuarios.txt | grep $dia | grep $mes | awk '{ print $1 }'`
+
+
+if [ $usuario = `` ]
+then
+echo "========================="
+echo "Nadie se conectó el $dia de $mes."
+echo "========================="
+else
+echo "========================="
+echo "El $dia de $mes se conectó: "
+echo $usuario
+echo "========================="
+fi
+
+
